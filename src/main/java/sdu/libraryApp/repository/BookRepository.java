56b30fcbabd0afd3e14ab58e243fcd321bd12d99
daisygith <daisygith@users.noreply.github.com>
2024-10-09 13:38:27 +0200

    delete some comments

--- src/main/java/sdu/libraryApp/repository/BookRepository.java
+++ src/main/java/sdu/libraryApp/repository/BookRepository.java
@@ -4,18 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import sdu.libraryApp.entity.Book;
 
-import java.util.List;
-import java.util.Optional;
-
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-
-//    List<Book> findAllBooks();
-//
-//    Optional<Book> findById(Long theId);
-//
-//    Book save(Book theBook);
-//
-//    void deleteById(Long theId);
-
 }
